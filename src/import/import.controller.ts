94cfbfe7c38dda7f5ae4d174de983ab39c4f17e1
Nicholas Lediet <nicklediet@gmail.com>
2024-03-15 14:34:37 -0400

    Update configs to interop with mono repo

--- src/import/import.controller.ts
+++ src/import/import.controller.ts
@@ -4,7 +4,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('import')
 export class ImportController {
     @Post()
-    @UseInterceptors(FileInterceptor('importFile'))
+    @UseInterceptors(FileInterceptor('file'))
     async import(@UploadedFile() file: Express.Multer.File) {
         const fileData = file.buffer.toString('utf8');
         console.log(fileData)
--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,21 @@ import { AppModule } from './app.module';
 import { Request } from 'express';
 
 const isOriginAllowed = (origin: string) => {
-  const allowList = ['localhost:5173']
+  const allowList = ['localhost:5173', 'localhost:3000']
   
   return allowList.indexOf(origin) !== -1
 }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
 
   app.enableCors((request: Request, callback) => {
     callback(null, {
-      origin: [/localhost\:5173/],
+      origin: [/localhost\:3000/],
       methods: ['GET', 'POST', 'DELTE', 'PUT']
     })
   })
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
